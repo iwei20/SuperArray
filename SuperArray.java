26d554c05572971e042c96a31e35dfe378b81255
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-11-01 14:38:12 -0500

    Change add() to return true always

--- SuperArray.java
+++ SuperArray.java
@@ -11,10 +11,11 @@ public class SuperArray {
         return size;
     }
 
-    public void add(String element) {
+    public boolean add(String element) {
         if(size >= data.length) resize();
         data[size] = element;
         ++size;
+        return true;
     }
 
     public String get(int index) {
