8d4bad28220d2f64fda932361ee03c89e81747a1
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-11-05 12:39:09 -0500

    Implement findOverlap()

--- SuperArray.java
+++ SuperArray.java
@@ -107,4 +107,13 @@ public class SuperArray {
             if(i != s.indexOf(s.get(i))) s.remove(i);
         }
     }
+
+    public static SuperArray findOverlap(SuperArray a, SuperArray b) {
+        SuperArray result = new SuperArray();
+        for(int i = 0; i < a.size(); ++i) {
+            if(b.contains(a.get(i))) result.add(a.get(i));
+        }
+        removeDuplicates(result);
+        return result;
+    }
 }
