868360674e75432399e0a5445785d7e0dc5666bc
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-11-07 21:57:01 -0500

    Implement and test zip()

--- Demo.java
+++ Demo.java
@@ -15,6 +15,16 @@ public class Demo {
         removeDuplicates(result);
         return result;
     }
+
+    public static SuperArray zip(SuperArray a, SuperArray b) {
+        SuperArray result = new SuperArray();
+        for(int i = 0; i < Math.max(a.size(), b.size()); ++i) {
+            if(i < a.size()) result.add(a.get(i));
+            if(i < b.size()) result.add(b.get(i));
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         SuperArray words = new SuperArray();
         System.out.println(words.isEmpty());
@@ -82,5 +92,7 @@ public class Demo {
         System.out.println(toCompare.lastIndexOf("B"));
         System.out.println(findOverlap(things, toCompare));
         System.out.println(findOverlap(things, toCompare).lastIndexOf("D"));
+        System.out.println(zip(things, findOverlap(things, toCompare)));
+        System.out.println(zip(new SuperArray(), new SuperArray()));
     }
 }
